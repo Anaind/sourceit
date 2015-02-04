f0697f8bd6f4af5a30ea6138b80b715429e991cd
Oleksii Bieliaiev <4n41nd@gmail.com>
2015-02-04 14:04:56 +0200

    Little style modification

--- Bieliaiev/ht8/main.js
+++ Bieliaiev/ht8/main.js
@@ -28,25 +28,22 @@ function matrix (size) {
 function pascal (size) {
     var triangle = [];
 
-    
         for (var i = 0; i <= size; i++) {
             triangle[i] = [];
-        };
+        }
         triangle[0][0] = 1;
         triangle[1][0] = 1;
         triangle[1][1] = 1;
         for (var i = 0; i <= size; i++) {
             triangle[i][0] = 1;
             triangle[i][i] = 1;
-        };
+        }
         
         for (var i = 1; i <= size; i++) {
             for (var j = 1; j < i; j++) {
                 triangle[i][j] = triangle[i - 1][j - 1] + triangle[i - 1][j];
-            };
-
-        };
-
+            }
+        }
     return triangle;
 }
 
